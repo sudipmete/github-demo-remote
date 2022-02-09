5ca4ef6b49417125b84ce9c99f2582a9ed0bce13
sudipmete <73578459+sudipmete@users.noreply.github.com>
2022-02-09 18:35:59 +0530

    initial commit on math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+#Add implementation
+def add(x,y):
+	return x+y
+    
+#Subtract implementation    
+def subtract(x,y):
+	return x-y              #on master branch
+    
+#Multiply implementation   
+def multiply(x,y):
+	return x*y              # on Bug456 branch
+    
+#Divide implementation    
+def divide(x,y):
+	if y==0:                # on master branch
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
+
+def square(x):
+    return x*x
