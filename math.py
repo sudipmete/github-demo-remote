ec2e16d85bb2d83cbb4a47ce559cba651500f7e0
sudipmete <73578459+sudipmete@users.noreply.github.com>
2022-02-09 18:43:22 +0530

    Update math.py

--- math.py
+++ math.py
@@ -1,21 +1,11 @@
-#Add implementation
 def add(x,y):
-	return x+y
-    
-#Subtract implementation    
+	pass
+       
 def subtract(x,y):
-	return x-y              #on master branch
-    
-#Multiply implementation   
+	pass
+       
 def multiply(x,y):
-	return x*y              # on Bug456 branch
+	pass   
     
-#Divide implementation    
 def divide(x,y):
-	if y==0:                # on master branch
-        return DIVIDE_BY_0_ERROR
-    else:
-        return x/y
-
-def square(x):
-    return x*x
+	pass
